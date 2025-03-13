281e06eaea3f0f89c2a4abc84f07b7f4f2dcd00d
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-03-13 11:30:39 +0700

    prosedur untuk input

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -15,5 +15,13 @@ void input() { //prosedur untuk input
     else{
         cout << "\nArray dapat mempunyai maksimal 20 elemen\n" << endl;
     }
-   
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen Array" << endl;
+    cout << "=====================" << endl;
+    for (int i = 0; i <n; i++){
+        cout << "data ke-" <<(i+1) << ";";
+        cin >> arr [i];
+    }
 }
