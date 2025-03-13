c25a688f3051d288376296e341c3fb8beb67501e
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-03-13 11:51:52 +0700

    prosedur  display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -39,3 +39,22 @@ void bubblesortArray(){
         pass = pass + 1; //step 4
     }while (pass <= n-1);//step 5
 }
+void display (){
+    cout << endl;
+    cout << "====================" endl;
+    cout << "elemen Array yang telah tersusun" endl;
+    cout << "====================" endl;
+    cout << endl;
+    for (int j = 0; j <n; j++){
+        cout << arr[j];//menampilkan array
+        if (j < n -1) {
+            cout << "-->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "jumlah pass = " << n-1 << endl;
+    cout << endl;
+    cout << endl;
+}
