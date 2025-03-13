d35241d29e3142b06f4143efacdaa4c3acf9f0f4
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id.>
2025-03-13 11:39:45 +0700

    bubblesort Array

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -25,3 +25,17 @@ void input() { //prosedur untuk input
         cin >> arr [i];
     }
 }
+void bubblesortArray(){
+    int pass = 1; //step 1
+
+    do {
+        for (int j= 0; j <= n-1-pass; j++){//step 2
+            if (arr[j] > arr[j+1]) {//step 3
+                int temp;
+                arr[j] = arr[j];
+                arr[j+1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4
+    }while (pass <= n-1);//step 5
+}
